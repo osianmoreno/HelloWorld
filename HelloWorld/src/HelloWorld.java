563de1df2fdb94ffafe4aef47902b11fe4a01983
osianmoreno <morenosoftware@gmail.com>
2013-10-29 16:42:42 +0100

    Tercer print

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,6 +1,8 @@
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("HelloWorld");
-		System.out.println("HelloWorld");
+		System.out.println("HelloWorld 1");
+		System.out.println("HelloWorld 2");
+		System.out.println("HelloWorld 3");
+		
 	}
 }

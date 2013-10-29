d856820eaa9b3939732692ed3c7ca15c62828a8e
osianmoreno <morenosoftware@gmail.com>
2013-10-29 16:55:32 +0100

    Nueva branca

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -2,7 +2,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("HelloWorld 1");
 		System.out.println("HelloWorld 2");
-		System.out.println("HelloWorld 3");
-		
+		System.out.println("HelloWorld 3");	
 	}
 }

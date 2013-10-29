94a434893981f63b772e5872e37b383e9d553a4a
osianmoreno <morenosoftware@gmail.com>
2013-10-29 17:00:07 +0100

    Solo un print

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,7 +1,5 @@
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("HelloWorld 1");
-		System.out.println("HelloWorld 2");
-		System.out.println("HelloWorld 3");	
 	}
 }
